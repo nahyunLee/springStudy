f7e92f358e6bb30cf3f98da7d950835e32a8849f
nahyunLee <sksms304@gmail.com>
2022-03-12 19:11:19 +0900

    회원 도메인과 레포지토리 만들기

---
+++ src/main/java/springExample/hellospring/domain/Member.java
@@ -0,0 +1,19 @@
+package springExample.hellospring.domain;
+
+public class Member {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
---
+++ src/main/java/springExample/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,38 @@
+package springExample.hellospring.repository;
+
+import springExample.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+    //여러곳에서 공유될 수 있으니까 원래는 동시성 생각해서 타입 고려해야함
+
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        //null이 될 가능성이 있으면 Optional로 감싸서 반환
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
